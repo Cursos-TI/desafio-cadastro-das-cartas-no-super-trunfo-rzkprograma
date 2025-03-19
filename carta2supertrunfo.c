938225bc410bdca2ff7ea190804f119040c2d71b
rzkprograma <pablookkj@gmail.com>
2025-03-19 13:58:22 +0000

    novo commit

--- carta2supertrunfo.c
+++ carta2supertrunfo.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
- 
 int main() {
     
-    char estado[20] = "Bahia";
-    char codigo [10]= "B01";
-    int populacao = 14.850;
-    float area = 567.295;
-    float pib = 425.5;
-    int pontosturisticos = 25;
+    char estado[10] = "SaoPaulo";
+    char codigo [10]= "S02";
+    int populacao = 46;
+    float area = 1.521;
+    float pib = 2.719;
+    int pontosturisticos = 28;
 
 
-    printf("estado 1: %s\n", estado);
+    printf("estado 2: %s\n", estado);
     printf("codigo: %s\n", codigo);
     printf("populacao: %d milhoes\n", populacao);
     printf("Area: %.2f metros km\n", area);
     printf("pib: %.2f bilhoes\n", pib);
     printf("pontos turisticos: %d pontos\n", pontosturisticos);
+
 
 }
