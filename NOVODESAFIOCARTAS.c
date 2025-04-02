c440c42d1899d1e9f450266f30a9dcd7abf31ba8
rzkprograma <pablookkj@gmail.com>
2025-04-02 14:13:48 +0000

    novo commit

---
+++ NOVODESAFIOCARTAS.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+// Estrutura para representar uma carta
+typedef struct {
+    char estado[50];
+    char codigo[10];
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+} Carta;
+
+// Função para comparar duas cartas com base na população
+void compararCartas(Carta c1, Carta c2) {
+    printf("\nComparação baseada em: População\n");
+    printf("%s (%s): %d habitantes\n", c1.cidade, c1.estado, c1.populacao);
+    printf("%s (%s): %d habitantes\n", c2.cidade, c2.estado, c2.populacao);
+
+    if (c1.populacao > c2.populacao) {
+        printf("Vencedora: sao paulo %s!\n", c1.cidade);
+    } else if (c1.populacao < c2.populacao) {
+        printf("Vencedora: Rio de Janeiro %s!\n", c2.cidade);
+    } else {
+        printf("Empate!\n");
+    }
+}
+
+int main() {
+    // Definição das cartas manualmente
+    Carta carta1 = {"São Paulo", "SP001", "São Paulo", 12300000, 1521.11, 699.28, 20};
+    Carta carta2 = {"Rio de Janeiro", "RJ001", "Rio de Janeiro", 6775561, 1200.27, 415.86, 15};
+    
+    compararCartas(carta1, carta2);
+
+    return 0;
+}
